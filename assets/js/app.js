136331574ee548fec117192a583a852eb77b15f0
Yelitza Cho <yelitza.choque@gmail.com>
2017-03-06 14:25:44 -0500

    Intentos de printl

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+function sprint2 (){
+  document.getElementById('sprint2').innerHTML= "<br> Sprint 2: Introducción al desarrollo web  construcción de páginas web </br>" +
+                                                "<br> 1. Ciclo de vida del desarrollo web y arquitectura básica cliente / servidor / Configuración del ambiente de trabajo e introducción a git.</br>" +
+                                                "<br> 2. HTML y Javascript en el navegador.</br>"
+                                                "<br> 3. Manejo de comandos básicos de git en la terminal.</br>"
+                                                "<br> 4. Pruebas Unitarias.</br>" +
+                                                "<br> 5. Git / Resolución de conflictos / Branching model.</br>" +
+                                                "<br> 6. DOM.</br>";
+
+}
