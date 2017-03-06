1fd0d718aeab37857310ce4090ea48bcf5ddf605
Yelitza Cho <yelitza.choque@gmail.com>
2017-03-06 17:10:27 -0500

    Modificando funciones

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,15 @@
+var mostarOcultar = function(divtexte) {
+	var divtext = document.getElementById(divtexte);
+	if (divtext.style.display == "block" ) {
+		divtext.style.display = "none";
+	} else{
+		divtext.style.display = "block";
+	}
+}
+
 document.getElementById("elBoton").onclick = function() {
   myFunction()
 };
-
 function myFunction() {
     document.getElementById("dropdown").classList.toggle("show");
-}
-
-
-function sprint2 (){
-  document.getElementById('sprint2').innerHTML= "<br> Sprint 2: Introducción al desarrollo web  construcción de páginas web </br>" +
-                                                "<br> 1. Ciclo de vida del desarrollo web y arquitectura básica cliente / servidor / Configuración del ambiente de trabajo e introducción a git.</br>" +
-                                                "<br> 2. HTML y Javascript en el navegador.</br>"
-                                                "<br> 3. Manejo de comandos básicos de git en la terminal.</br>"
-                                                "<br> 4. Pruebas Unitarias.</br>" +
-                                                "<br> 5. Git / Resolución de conflictos / Branching model.</br>" +
-                                                "<br> 6. DOM.</br>";
-
 }
